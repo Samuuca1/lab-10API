07d8d2fb7f08cec80a13f532c98a603179f9b3d0
Samuuca1 <samuelchaves_2012@hotmail.com>
2025-08-09 20:53:14 -0400

    Get requested implemented using fetch

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById('fetchBtn').addEventListener('click', getFetch);
+
+
+//Part1
+function getFeetch() {
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+        .then(res => {
+            if (!res.ok) throw new Error('Fetch request failed');
+            return res.json();
+        })
+        .then(data => {
+            document.getElementById('output').innerHTML = `
+                <h3>${data.title}</h3>
+                <p>${data.body}</p>
+            `;
+        })
+        .catch(err => {
+            document.getElementById('output').innerHTML = `<p style="color:red;">${err}</p>`;
+        });
+}
