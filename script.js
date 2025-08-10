2e8801320de7fa08b31a91b2eb3fcc9e27c61005
Samuuca1 <samuelchaves_2012@hotmail.com>
2025-08-09 21:22:05 -0400

    API interaction using XML added to the JS file

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('fetchBtn').addEventListener('click', getFetch);
-
+document.getElementById('xhrBtn').addEventListener('click', getDataXHR);
 
 //Part1
 function getFetch() {
@@ -17,4 +17,26 @@ function getFetch() {
         .catch(err => {
             document.getElementById('output').innerHTML = `<p style="color:red;">${err}</p>`;
         });
+}
+
+
+//Part2
+function getDataXHR() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts/2');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            const data = JSON.parse(xhr.responseText);
+            document.getElementById('output').innerHTML = `
+                <h3>${data.title}</h3>
+                <p>${data.body}</p>
+            `;
+        } else {
+            document.getElementById('output').innerHTML = `<p style="color:red;">XHR request failed</p>`;
+        }
+    };
+    xhr.onerror = function() {
+        document.getElementById('output').innerHTML = `<p style="color:red;">Network Error</p>`;
+    };
+    xhr.send();
 }
